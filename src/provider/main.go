c9f881cf39eae009c4ec56aeedea2d4469914df1
shaojintian <sjt@hnu.edu.cn>
2019-11-29 00:34:49 +0800

    Update main.go

--- src/provider/main.go
+++ src/provider/main.go
@@ -1,21 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
-
 
 func main() {
-	// init reverseProxy http handler
-	provider := http.Server{
-		Addr: fmt.Sprintf("10.69.253.170:%d", 4001),
-		//Handler: http.HandlerFunc(sp.LoadBalance),
-	}
 
-	log.Println("start provider[%s].ListenAndServe--------------------------------",provider.Addr)
-	if err := provider.ListenAndServe();err != nil {
-		log.Fatal(err.Error())
-	}
 }
